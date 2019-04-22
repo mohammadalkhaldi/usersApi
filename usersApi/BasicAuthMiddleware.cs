4795133dcbf0860ec80e08a1a58ce10bb67a60d0
mohammadalkhaldi <mohammad.alkhaldi303@gmail.com>
2019-04-22 22:49:51 +0900

    Add Basic Authentication

---
+++ usersApi/BasicAuthMiddleware.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace usersApi
+{
+    public class BasicAuthMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly string _realm;
+
+        public BasicAuthMiddleware(RequestDelegate next, string realm)
+        {
+            _next = next;
+            _realm = realm;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string authHeader = context.Request.Headers["Authorization"];
+            if (authHeader != null && authHeader.StartsWith("Basic "))
+            {
+                
+                var encodedUsernamePassword = authHeader.Split(' ', 2, StringSplitOptions.RemoveEmptyEntries)[1]?.Trim();
+                
+                var decodedUsernamePassword = Encoding.UTF8.GetString(Convert.FromBase64String(encodedUsernamePassword));
+                
+                var username = decodedUsernamePassword.Split(':', 2)[0];
+                var password = decodedUsernamePassword.Split(':', 2)[1];
+                
+                if (IsAuthorized(username, password))
+                {
+                    await _next.Invoke(context);
+                    return;
+                }
+            }
+            // Return authentication type (causes browser to show login dialog)
+            context.Response.Headers["WWW-Authenticate"] = "Basic";
+            // Add realm if it is not null
+            if (!string.IsNullOrWhiteSpace(_realm))
+            {
+                context.Response.Headers["WWW-Authenticate"] += $" realm=\"{_realm}\"";
+            }
+            // Return unauthorized
+            context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+        }
+
+        
+        public bool IsAuthorized(string username, string password)
+        {
+
+            var basicAuthUserName = "mohammadalk";
+            var basicAuthPassword = "dingding345";
+      
+            return username.Equals(basicAuthUserName, StringComparison.InvariantCultureIgnoreCase)
+                   && password.Equals(basicAuthPassword);
+           
+        }
+    }
+}
--- usersApi/Startup.cs
+++ usersApi/Startup.cs
@@ -47,6 +47,7 @@ namespace usersApi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseMiddleware<BasicAuthMiddleware>("MYAPI");
             try
             {
                 UserService user = new UserService(Configuration);
